90e316b62034fee2a0c99d7caa9f68508ab8e6b7
wolfdan666 <wolfdan666666@gmail.com>
2019-07-14 23:42:51 +0800

    周日 2019/07/14

---
+++ 2019.7/2019.7.14/leetcode703_kLagerest.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class KthLargest {
+public:
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int size;
+
+    KthLargest(int k, vector<int> nums) {
+        size=k;//将小根堆的大小控制在k
+        for(int i=0;i<nums.size();i++) {
+            pq.push(nums[i]);
+            if(pq.size()>k) pq.pop();
+        }
+    }
+    
+    int add(int val) {
+        pq.push(val);
+        if(pq.size()>size) pq.pop();
+        return pq.top();
+    }
+};
