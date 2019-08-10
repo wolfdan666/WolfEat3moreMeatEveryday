22a9ee0bfb0e9b7ae9cfc2ea3990ee6eee651052
wolfdan666 <wolfdan666666@gmail.com>
2019-08-11 01:56:13 +0800

    周日 2019/08/11

--- 2019.8/2019.8.10/N8B_ldm区间贡献_期望线性性.cpp
+++ 2019.8/2019.8.10/N8B_ldm区间贡献_期望线性性.cpp
@@ -1,8 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-const int mod = 1e9 + 7;
-const int inf = 0x3f3f3f3f;
 const int maxn = 100000 + 5;
 
 int a[maxn], b[maxn], c[maxn];
@@ -16,14 +14,8 @@ int main() {
         b[a[i]] = i;
     }
     ll ans = 0;
-    for(int i=1;i<=n;i++) cout<<c[i]<<" "; cout<<endl;
     for(int i = 1; i <= n; i++) {
-        /*最后一项表示前面的区间的贡献量
-        前面一项表示*/
         ans += 1LL*(n-i+1)*(i-c[i]+1);
-        cout<<(n-i+1)<<" ";
-        cout<<(i-c[i]+1)<<" ";
-        cout<<(n-i+1)*(i-c[i]+1)<<endl;
     }
     printf("%lld\n", ans);
 
