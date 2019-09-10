13678db771e8cd19a8490473714ac6bd69060af2
wolfdan <862445752@qq.com>
2019-09-10 17:17:39 +0800

    %date%

---
+++ 2019.9/2019.9.10/hello_deepin_sublime.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	printf("%s\n","厉Hello World哥！" );
+	return 0;
+}
---
+++ git.sh
@@ -0,0 +1,4 @@
+git pull suanfa master
+git add --all
+git commit -m "%date%"
+git push suanfa master
