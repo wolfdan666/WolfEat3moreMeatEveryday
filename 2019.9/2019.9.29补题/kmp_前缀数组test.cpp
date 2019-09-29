2cb36bea43f0572e4306eb81791971975a640432
wolfdan <862445752@qq.com>
2019-09-29 23:23:13 +0800

    2019-09-29 23:23:13

---
+++ 2019.9/2019.9.29补题/kmp_前缀数组test.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll mod=1000000007;
+const int inf = 0x3f3f3f3f;
+inline int rd() {int x=0;char p=getchar();while(p<'0'||p>'9')p=getchar();while(p>='0'&&p<='9')x=x*10+p-'0',p=getchar();return x;}
+string s;
+
+vector<int> prefix_function(string s) {
+  int n = (int)s.length();
+  vector<int> pi(n);
+  for (int i = 1; i < n; i++) {
+    int j = pi[i - 1];
+    while (j > 0 && s[i] != s[j]) j = pi[j - 1];
+    if (s[i] == s[j]) j++;
+    pi[i] = j;
+  }
+  return pi;
+}
+
+int main(){
+	while(cin>>s){
+		vector<int> t = prefix_function(s);
+		for(auto i:t) cout<<i<<" "; cout<<endl;
+	}
+
+    return 0;
+}
