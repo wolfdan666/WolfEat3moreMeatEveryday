93bef5e8eb19958902771bb108c4cd875d2f950d
wolfdan <862445752@qq.com>
2019-09-30 20:35:22 +0800

    2019-09-30 20:35:22

--- 2019.9/2019.9.29CF_589/B.cpp
+++ 2019.9/2019.9.29CF_589/B.cpp
@@ -25,22 +25,23 @@ lxc大佬让我直接使用2^n^2 的暴力对拍,果然大佬
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+#define rep(i, a, b) for(int i = int(a); i <= int(b); ++i)
+#define per(i, b, a) for(int i = int(b); i >= int(a); --i)
+#define mem(x, y) memset(x, y, sizeof(x))
+#define SZ(x) x.size()
+#define mk make_pair
+#define pb push_back
+#define fi first
+#define se second
 const ll mod=1000000007;
 const int inf = 0x3f3f3f3f;
 inline int rd() {int x=0;char p=getchar();while(p<'0'||p>'9')p=getchar();while(p>='0'&&p<='9')x=x*10+p-'0',p=getchar();return x;}
+inline ll qpow(ll a,ll b){ll ans=1%mod;for(;b;b>>=1){if(b&1)ans=ans*a%mod;a=a*a%mod;}return ans;}
 const int M = 1e3+10;
 int h,w;
 int r[M],c[M];
 int cnt[M][M];
 
-int qpow(int a,int b){
-	int ans = 1;
-	for(;b;b>>=1){
-		if(b&1) ans = (ll) ans*a%mod;
-		a = (ll) a*a%mod;
-	}
-	return ans;
-}
 
 int main(){
 	h=rd();w=rd();
