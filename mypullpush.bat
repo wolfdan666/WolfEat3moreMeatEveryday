41c8629baf064d4914df7fb9671910b4d76df6d3
wolfdan666 <wolfdan666666@gmail.com>
2019-10-05 16:42:30 +0800

    2019/10/05 周六

--- mypullpush.bat
+++ mypullpush.bat
@@ -1,9 +1,8 @@
-git pull suanfa master
+﻿git pull suanfa master
 git add --all
 git commit -m "%date%"
 git push suanfa master
 git push suanfa@gitee master
 # 暂时未尝试
-cd ..
 del /s /q *.exe
 # del 是删除命令, *.exe指的是所有的后缀为exe的文件. 还有两个参数/s代表包含该目录下所有的子目录. /q代表不询问, 也就是直接删除.
