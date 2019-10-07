2e6b8f72a92667b0e950396a2e7c8827a3d25f01
wolfdan <862445752@qq.com>
2019-10-07 22:09:53 +0800

    2019-10-07 22:09:53

---
+++ 2019.10/2019.10.7/test.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, a, b) for(int i = int(a); i <= int(b); ++i)
+#define per(i, b, a) for(int i = int(b); i >= int(a); --i)
+#define mem(x, y) memset(x, y, sizeof(x))
+#define SZ(x) x.size()
+#define mk make_pair
+#define pb push_back
+#define fi first
+#define se second
+const ll mod=1000000007;
+const int inf = 0x3f3f3f3f;
+inline int rd(){char c=getchar();int x=0,f=1;while(c<'0'||c>'9'){if(c=='-')f=-1;c=getchar();}while(c>='0'&&c<='9'){x=x*10+c-'0';c=getchar();}return x*f;}
+inline ll qpow(ll a,ll b){ll ans=1%mod;for(;b;b>>=1){if(b&1)ans=ans*a%mod;a=a*a%mod;}return ans;}
+
+
+int main(){
+	int N = 100;
+	for (int i = 1; i < N; i |= i + 1){
+		cout<<i<<" \n"[i%10==0];
+	}
+	cout<<"\n-----------"<<endl;
+	for (int i = 2; i < N; i |= i + 1){
+		cout<<i<<" \n"[i%10==0];
+	}
+
+	/*
+	1 3 7 15 31 63 
+	-----------
+	2 3 7 15 31 63 
+	*/
+
+
+    return 0;
+}
