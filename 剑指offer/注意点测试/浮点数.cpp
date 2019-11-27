1203366bea46a2a759ad65b471f666f29e05657c
wolfdan666 <wolfdan666666@gmail.com>
2019-11-27 21:58:20 +0800

    2019/11/27 周三

---
+++ 剑指offer/注意点测试/浮点数.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, a, b) for(int i = int(a); i <= int(b); ++i)
+#define per(i, b, a) for(int i = int(b); i >= int(a); --i)
+#define mem(x, y) memset(x, y, sizeof(x))
+#define SZ(x) x.size()
+#define mk make_pair
+#define pb push_back
+#define fi first
+#define se second
+const ll mod=1000000007;
+const int inf = 0x3f3f3f3f;
+inline int rd(){char c=getchar();int x=0,f=1;while(c<'0'||c>'9'){if(c=='-')f=-1;c=getchar();}while(c>='0'&&c<='9'){x=x*10+c-'0';c=getchar();}return x*f;}
+inline ll qpow(ll a,ll b){ll ans=1%mod;for(;b;b>>=1){if(b&1)ans=ans*a%mod;a=a*a%mod;}return ans;}
+double a,b;
+
+bool Equal(double a,double b){
+    if(abs(a-b) < double(1e-7)) return true;
+    else return false;
+}
+
+int main(){
+    const double esp = 1e-7;
+    const double esp2 = 0.0000001;
+    cout<<esp<<" "<<esp2<<endl; // 是一样的
+
+    int T;
+    cin>>T;
+    while(T--){
+        cin>>a>>b;
+        cout<<Equal(a,b)<<endl;
+    }
+
+    return 0;
+}
