fa5f3a8b81fea81fc7b13f445b86df48833d1b3f
wolfdan666 <wolfdan666666@gmail.com>
2019-12-29 12:45:28 +0800

    2019/12/29 周日

--- 剑指offer/各种排序/选择排序.cpp
+++ 剑指offer/各种排序/选择排序.cpp
@@ -14,6 +14,8 @@ const int inf = 0x3f3f3f3f;
 inline int rd(){char c=getchar();int x=0,f=1;while(c<'0'||c>'9'){if(c=='-')f=-1;c=getchar();}while(c>='0'&&c<='9'){x=x*10+c-'0';c=getchar();}return x*f;}
 inline ll qpow(ll a,ll b){ll ans=1%mod;for(;b;b>>=1){if(b&1)ans=ans*a%mod;a=a*a%mod;}return ans;}
 
+// case : {5,5,2} --- 第一次交换了 0,2 下标的元素，破坏了相等元素之间的相对位置，所以不稳定
+
 // 不稳定,因为两个相同的值,后面的值排序后排到了前面去了
 template<typename T> //整数或浮点数皆可使用，若要使用物件（class）时必须设定大于（>）的运算子功能
 void selection_sort(std::vector<T>& arr) {
