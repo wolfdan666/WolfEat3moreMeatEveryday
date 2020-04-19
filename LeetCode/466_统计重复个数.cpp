3cc65fb9eb701cb4f157275b5f0f40963672a507
wolfdan666 <wolfdan666666@gmail.com>
2020-04-19 13:25:45 +0800

    2020/04/19 周日

---
+++ LeetCode/466_统计重复个数.cpp
@@ -0,0 +1,126 @@
+/*
+2020年4月19日12:08:51 开始看题
+
+2020年4月19日12:13:06 思考：
+n1 > n2, 应该是可以直接处理两个简单串的
+acb
+abab
+---> 那么就要两倍acb先,主要难点应该在这种case里面
+
+所以先对比S1和S2的char_count是否满足要求
+即看[S2,1]能不能从S1中获得
+
+这里既然是求最大的话，那么必然存在这样一种贪心
+那就是通过叠加s1->S1的过程中，看看s2是否能满足
+找到恰好满足的点，然后就好算出了
+既然大家都在100个char,那么就记录这100个count_char的记录
+count[26][100][100],然后遍历[(1->n1)*s1,k]去找满足的
+tmp[26][100][100]去对比..
+然后再对个数进行操作,感觉可能不是正解，会漏conor case
+而且这种方法好像会卡TLE或者MLE
+所以，2020年4月19日12:30:36 看tutorial
+
+看了题解，觉得自己不配说自己曾经是负责字符串的acmer
+
+
+对于tutorial中说的一定会有循环节我是不认同的，因为
+万一s2中有s1中不存在的字符,那S1不可能通过删除获得s2
+
+
+其实我的思路方向也是有点点正确的，只是和题解差了很多火候
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, a, b) for(int i = int(a); i <= int(b); ++i)
+#define per(i, b, a) for(int i = int(b); i >= int(a); --i)
+#define mem(x, y) memset(x, y, sizeof(x))
+#define SZ(x) x.size()
+#define mk make_pair
+#define pb push_back
+#define fi first
+#define se second
+const ll mod=1000000007;
+const int inf = 0x3f3f3f3f;
+inline int rd(){char c=getchar();int x=0,f=1;while(c<'0'||c>'9'){if(c=='-')f=-1;c=getchar();}while(c>='0'&&c<='9'){x=x*10+c-'0';c=getchar();}return x*f;}
+inline ll qpow(ll a,ll b){ll ans=1%mod;for(;b;b>>=1){if(b&1)ans=ans*a%mod;a=a*a%mod;}return ans;}
+class Solution {
+public:
+    int getMaxRepetitions(string s1, int n1, string s2, int n2) {
+        if (n1 == 0) {
+            return 0;
+        }
+        int s1cnt = 0, index = 0, s2cnt = 0;
+        // recall 是我们用来找循环节的变量，它是一个哈希映射
+        // 我们如何找循环节？假设我们遍历了 s1cnt 个 s1，此时匹配到了第 s2cnt 个 s2 中的第 index 个字符
+        // 如果我们之前遍历了 s1cnt' 个 s1 时，匹配到的是第 s2cnt' 个 s2 中同样的第 index 个字符，那么就有循环节了
+        // 我们用 (s1cnt', s2cnt', index) 和 (s1cnt, s2cnt, index) 表示两次包含相同 index 的匹配结果
+        // 那么哈希映射中的键就是 index，值就是 (s1cnt', s2cnt') 这个二元组
+        // 循环节就是；
+        //    - 前 s1cnt' 个 s1 包含了 s2cnt' 个 s2
+        //    - 以后的每 (s1cnt - s1cnt') 个 s1 包含了 (s2cnt - s2cnt') 个 s2
+        // 那么还会剩下 (n1 - s1cnt') % (s1cnt - s1cnt') 个 s1, 我们对这些与 s2 进行暴力匹配
+        // 注意 s2 要从第 index 个字符开始匹配
+        unordered_map<int, pair<int, int>> recall;
+        pair<int, int> pre_loop, in_loop;
+        while (true) {
+            // 我们多遍历一个 s1，看看能不能找到循环节
+            ++s1cnt;
+            for (char ch: s1) {
+                if (ch == s2[index]) {
+                    index += 1;
+                    if (index == s2.size()) {
+                        ++s2cnt;
+                        index = 0;
+                    }
+                }
+            }
+            // 还没有找到循环节，所有的 s1 就用完了
+            if (s1cnt == n1) {
+                return s2cnt / n2;
+            }
+            // 出现了之前的 index，表示找到了循环节
+            // 这里是先进入else 的
+            if (recall.count(index)) {
+                auto [s1cnt_prime, s2cnt_prime] = recall[index];
+                // 前 s1cnt' 个 s1 包含了 s2cnt' 个 s2
+                pre_loop = {s1cnt_prime, s2cnt_prime};
+                // 以后的每 (s1cnt - s1cnt') 个 s1 包含了 (s2cnt - s2cnt') 个 s2
+                in_loop = {s1cnt - s1cnt_prime, s2cnt - s2cnt_prime};
+                break;
+            }
+            else {
+                recall[index] = {s1cnt, s2cnt};
+            }
+        }
+        // ans 存储的是 S1 包含的 s2 的数量，考虑的之前的 pre_loop 和 in_loop
+        int ans = pre_loop.second + (n1 - pre_loop.first) / in_loop.first * in_loop.second;
+        // S1 的末尾还剩下一些 s1，我们暴力进行匹配
+        int rest = (n1 - pre_loop.first) % in_loop.first;
+        for (int i = 0; i < rest; ++i) {
+            for (char ch: s1) {
+                if (ch == s2[index]) {
+                    ++index;
+                    if (index == s2.size()) {
+                        ++ans;
+                        index = 0;
+                    }
+                }
+            }
+        }
+        // S1 包含 ans 个 s2，那么就包含 ans / n2 个 S2
+        return ans / n2;
+    }
+};
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/count-the-repetitions/solution/tong-ji-zhong-fu-ge-shu-by-leetcode-solution/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+int main(){
+
+
+    return 0;
+}
