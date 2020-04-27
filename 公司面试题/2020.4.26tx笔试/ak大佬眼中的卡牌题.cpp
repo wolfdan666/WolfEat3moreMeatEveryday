f023d1f217ba06cf6da10585eb707888ab924a7c
wolfdan666 <wolfdan666666@gmail.com>
2020-04-28 00:10:46 +0800

    2020/04/28 周二

--- 公司面试题/2020.4.26tx笔试/ak大佬眼中的卡牌题.cpp
+++ 公司面试题/2020.4.26tx笔试/ak大佬眼中的卡牌题.cpp
@@ -8,6 +8,19 @@
 状压dp , 不过要预处理下 ，将偶数下标的 a_i, b_i ,swap 。
 跑子集dp ，我的更新是 dp[S][k]=min(dp[S][k],dp[S-k][j]+合法的) k属于 S集合
 有点不好描述啊
+
+
+我问大佬详细思路：
+作者：FranklinShan
+链接：https://www.nowcoder.com/discuss/417957?toCommentId=6011683
+来源：牛客网
+
+假入 最后的方案是 a[t[1]],a[t[2]],...a[t[n]]  ， t[i] 是下标  ，
+有一个性质是  第i张牌正面朝上，当且仅当 2| (i-t[i]) ，( 这个可以手动玩一下可以发现)
+也就是原来的位置和现在的位置差是偶数。 所以可以考虑状压dp，
+设dp[S][k] 表示S这个集合中的卡片已经选好了位置，然后最后一个数是K,
+然后枚举当前填的数是 p，所以可以知道 p卡片朝上的数是 j,
+所以 dp[S][j]=min(dp[S-{p}][k]+num) ,num是 p放下 后产生的逆序对个数。
 */
 
 #include<bits/stdc++.h>
@@ -46,6 +59,7 @@ int main(){
             if(dp[i][j]==INF)continue;
             for(int k=0;k<n;k++){
                 if(i>>k&1)continue;
+                // 2020年4月27日23:52:55 这里的状压30分钟还是没有看懂，明天继续
                 int x=__builtin_popcount(i)&1;
                 if(x){
                     if(b[k]<a[j])continue;
