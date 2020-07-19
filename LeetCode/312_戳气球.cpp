21a487d95d758bf5c368bd75400414adebab8117
wolfdan666 <wolfdan666666@gmail.com>
2020-07-19 17:26:32 +0800

    2020/07/19 周日

---
+++ LeetCode/312_戳气球.cpp
@@ -0,0 +1,97 @@
+/*
+2020年7月19日16:39:48 开始看题
+
+2020年7月19日16:44:04 感觉最优解应该会是戳中一个后
+连续戳左右两边的某个气球,所以应该最大复杂度是 `n*(2^n)`
+
+2020年7月19日16:59:29 还是看题解吧
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, a, b) for(int i = int(a); i <= int(b); ++i)
+#define per(i, b, a) for(int i = int(b); i >= int(a); --i)
+#define mem(x, y) memset(x, y, sizeof(x))
+#define SZ(x) x.size()
+#define mk make_pair
+#define pb push_back
+#define fi first
+#define se second
+const ll mod=1000000007;
+const int inf = 0x3f3f3f3f;
+inline int rd(){char c=getchar();int x=0,f=1;while(c<'0'||c>'9'){if(c=='-')f=-1;c=getchar();}while(c>='0'&&c<='9'){x=x*10+c-'0';c=getchar();}return x*f;}
+inline ll qpow(ll a,ll b){ll ans=1%mod;for(;b;b>>=1){if(b&1)ans=ans*a%mod;a=a*a%mod;}return ans;}
+
+class Solution {
+public:
+    vector<vector<int>> rec;
+    vector<int> val;
+
+public:
+    int solve(int left, int right) {
+        if (left >= right - 1) {
+            return 0;
+        }
+        // 记忆搜索
+        if (rec[left][right] != -1) {
+            return rec[left][right];
+        }
+        for (int i = left + 1; i < right; i++) {
+            int sum = val[left] * val[i] * val[right];
+            sum += solve(left, i) + solve(i, right);
+            rec[left][right] = max(rec[left][right], sum);
+        }
+        return rec[left][right];
+    }
+
+    int maxCoins(vector<int>& nums) {
+        int n = nums.size();
+        val.resize(n + 2);
+        for (int i = 1; i <= n; i++) {
+            val[i] = nums[i - 1];
+        }
+        val[0] = val[n + 1] = 1;
+        rec.resize(n + 2, vector<int>(n + 2, -1));
+        return solve(0, n + 1);
+    }
+};
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/burst-balloons/solution/chuo-qi-qiu-by-leetcode-solution/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> rec(n + 2, vector<int>(n + 2));
+        vector<int> val(n + 2);
+        val[0] = val[n + 1] = 1;
+        for (int i = 1; i <= n; i++) {
+            val[i] = nums[i - 1];
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 2; j <= n + 1; j++) {
+                for (int k = i + 1; k < j; k++) {
+                    int sum = val[i] * val[k] * val[j];
+                    sum += rec[i][k] + rec[k][j];
+                    rec[i][j] = max(rec[i][j], sum);
+                }
+            }
+        }
+        return rec[0][n + 1];
+    }
+};
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/burst-balloons/solution/chuo-qi-qiu-by-leetcode-solution/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+int main(){
+
+
+    return 0;
+}
