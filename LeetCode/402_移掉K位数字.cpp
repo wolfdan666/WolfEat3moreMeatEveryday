7e41cc5cbb8bdc1760c9c9f4fabfc855db09362d
linmin <shanlinmin@qq.com>
2020-11-15 23:17:30 +0800

    2020-11-15 23:17:30

---
+++ LeetCode/402_移掉K位数字.cpp
@@ -0,0 +1,62 @@
+/*
+2020年11月15日22:56:00 开始看题解，因为想了比较久，但是没想到
+
+链接：https://leetcode-cn.com/problems/remove-k-digits/solution/yi-diao-kwei-shu-zi-by-leetcode-solution/
+
+2020年11月15日23:13:32 单调栈牛逼
+
+考虑到栈的特点是后进先出，如果通过栈实现，则需要将栈内元素依次弹出然后进行翻转才能得到最小数。
+为了避免翻转操作，可以使用双端队列代替栈的实现
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, a, b) for(int i = int(a); i <= int(b); ++i)
+#define per(i, b, a) for(int i = int(b); i >= int(a); --i)
+#define mem(x, y) memset(x, y, sizeof(x))
+#define SZ(x) x.size()
+#define mk make_pair
+#define pb push_back
+#define fi first
+#define se second
+const ll mod=1000000007;
+const int inf = 0x3f3f3f3f;
+inline int rd(){char c=getchar();int x=0,f=1;while(c<'0'||c>'9'){if(c=='-')f=-1;c=getchar();}while(c>='0'&&c<='9'){x=x*10+c-'0';c=getchar();}return x*f;}
+inline ll qpow(ll a,ll b){ll ans=1%mod;for(;b;b>>=1){if(b&1)ans=ans*a%mod;a=a*a%mod;}return ans;}
+
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        vector<char> stk;
+        for (auto& digit: num) {
+            while (stk.size() > 0 && stk.back() > digit && k) {
+                stk.pop_back();
+                k -= 1;
+            }
+            stk.push_back(digit);
+        }
+
+        for (; k > 0; --k) {
+            stk.pop_back();
+        }
+
+        string ans = "";
+        bool isLeadingZero = true;
+        for (auto& digit: stk) {
+            if (isLeadingZero && digit == '0') {
+                continue;
+            }
+            isLeadingZero = false;
+            ans += digit;
+        }
+        return ans == "" ? "0" : ans;
+    }
+};
+
+
+int main(){
+
+
+    return 0;
+}
