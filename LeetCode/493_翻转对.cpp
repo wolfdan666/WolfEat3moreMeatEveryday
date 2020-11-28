029e54aff05d0d738e91b0563bf7152d691685b8
linmin <shanlinmin@qq.com>
2020-11-28 21:32:36 +0800

    493-翻转树-区间范围-树状数组or归并排序

---
+++ LeetCode/493_翻转对.cpp
@@ -0,0 +1,135 @@
+/*
+2020年11月28日20:52:03 开始看题
+
+感觉这题的做法是倒序遍历，然后用一个线段树或者那个叫什么去的东西，就是用`x & -x`的那个
+全局搜索了一下，才想起来是树状数组，看来真的是好久没有刷题
+
+看答案吧---看答案发现果然和327区间和的个数 那个题目是类似的，解法也完全类似
+链接：https://leetcode-cn.com/problems/reverse-pairs/solution/fan-zhuan-dui-by-leetcode-solution/
+
+2020年11月28日21:29:37 树状数组牛逼，居然用了38分钟，菜了
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, a, b) for(int i = int(a); i <= int(b); ++i)
+#define per(i, b, a) for(int i = int(b); i >= int(a); --i)
+#define mem(x, y) memset(x, y, sizeof(x))
+#define SZ(x) x.size()
+#define mk make_pair
+#define pb push_back
+#define fi first
+#define se second
+const ll mod=1000000007;
+const int inf = 0x3f3f3f3f;
+inline int rd(){char c=getchar();int x=0,f=1;while(c<'0'||c>'9'){if(c=='-')f=-1;c=getchar();}while(c>='0'&&c<='9'){x=x*10+c-'0';c=getchar();}return x*f;}
+inline ll qpow(ll a,ll b){ll ans=1%mod;for(;b;b>>=1){if(b&1)ans=ans*a%mod;a=a*a%mod;}return ans;}
+
+// 归并排序解法
+class Solution_partion {
+public:
+    int reversePairsRecursive(vector<int>& nums, int left, int right) {
+        if (left == right) {
+            return 0;
+        } else {
+            int mid = (left + right) / 2;
+            int n1 = reversePairsRecursive(nums, left, mid);
+            int n2 = reversePairsRecursive(nums, mid + 1, right);
+            int ret = n1 + n2;
+
+            // 首先统计下标对的数量
+            int i = left;
+            int j = mid + 1;
+            while (i <= mid) {
+                while (j <= right && (long long)nums[i] > 2 * (long long)nums[j]) j++;
+                ret += (j - mid - 1);
+                i++;
+            }
+
+            // 随后合并两个排序数组
+            vector<int> sorted(right - left + 1);
+            int p1 = left, p2 = mid + 1;
+            int p = 0;
+            while (p1 <= mid || p2 <= right) {
+                if (p1 > mid) {
+                    sorted[p++] = nums[p2++];
+                } else if (p2 > right) {
+                    sorted[p++] = nums[p1++];
+                } else {
+                    if (nums[p1] < nums[p2]) {
+                        sorted[p++] = nums[p1++];
+                    } else {
+                        sorted[p++] = nums[p2++];
+                    }
+                }
+            }
+            for (int i = 0; i < sorted.size(); i++) {
+                nums[left + i] = sorted[i];
+            }
+            return ret;
+        }
+    }
+
+    int reversePairs(vector<int>& nums) {
+        if (nums.size() == 0) return 0;
+        return reversePairsRecursive(nums, 0, nums.size() - 1);
+    }
+};
+
+class Solution {
+public:
+    int reversePairsRecursive(vector<int>& nums, int left, int right) {
+        if (left == right) {
+            return 0;
+        } else {
+            int mid = (left + right) / 2;
+            int n1 = reversePairsRecursive(nums, left, mid);
+            int n2 = reversePairsRecursive(nums, mid + 1, right);
+            int ret = n1 + n2;
+
+            // 首先统计下标对的数量
+            int i = left;
+            int j = mid + 1;
+            while (i <= mid) {
+                while (j <= right && (long long)nums[i] > 2 * (long long)nums[j]) j++;
+                ret += (j - mid - 1);
+                i++;
+            }
+
+            // 随后合并两个排序数组
+            vector<int> sorted(right - left + 1);
+            int p1 = left, p2 = mid + 1;
+            int p = 0;
+            while (p1 <= mid || p2 <= right) {
+                if (p1 > mid) {
+                    sorted[p++] = nums[p2++];
+                } else if (p2 > right) {
+                    sorted[p++] = nums[p1++];
+                } else {
+                    if (nums[p1] < nums[p2]) {
+                        sorted[p++] = nums[p1++];
+                    } else {
+                        sorted[p++] = nums[p2++];
+                    }
+                }
+            }
+            for (int i = 0; i < sorted.size(); i++) {
+                nums[left + i] = sorted[i];
+            }
+            return ret;
+        }
+    }
+
+    int reversePairs(vector<int>& nums) {
+        if (nums.size() == 0) return 0;
+        return reversePairsRecursive(nums, 0, nums.size() - 1);
+    }
+};
+
+
+int main(){
+
+
+    return 0;
+}
