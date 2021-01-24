ed04a7b59d8d59c2f854bb8006544aef0cb8fe80
shanlinmin <shanlinmin@qq.com>
2021-01-24 22:14:34 +0800

    第90周打卡的简单遍历题

---
+++ LeetCode/674_最长连续递增序列.cpp
@@ -0,0 +1,43 @@
+/* 
+2021年01月24日22:09:27 感觉就是一个很简单的O(n)遍历计数，对的
+
+
+ */
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, a, b) for(int i = int(a); i <= int(b); ++i)
+#define per(i, b, a) for(int i = int(b); i >= int(a); --i)
+#define mem(x, y) memset(x, y, sizeof(x))
+#define SZ(x) x.size()
+#define mk make_pair
+#define pb push_back
+#define fi first
+#define se second
+const ll mod=1000000007;
+const int inf = 0x3f3f3f3f;
+inline int rd(){char c=getchar();int x=0,f=1;while(c<'0'||c>'9'){if(c=='-')f=-1;c=getchar();}while(c>='0'&&c<='9'){x=x*10+c-'0';c=getchar();}return x*f;}
+inline ll qpow(ll a,ll b){ll ans=1%mod;for(;b;b>>=1){if(b&1)ans=ans*a%mod;a=a*a%mod;}return ans;}
+class Solution {
+public:
+    int findLengthOfLCIS(vector<int>& nums) {
+        int ans = 0;
+        int n = nums.size();
+        int start = 0;
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] <= nums[i - 1]) {
+                start = i;
+            }
+            ans = max(ans, i - start + 1);
+        }
+        return ans;
+    }
+};
+
+
+int main(){
+    
+    
+    return 0;
+}
+
