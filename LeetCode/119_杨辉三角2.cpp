9d0c0aef1ac164bac5388041db4e479103b97ec7
shanlinmin <shanlinmin@qq.com>
2021-02-12 14:13:53 +0800

    119_杨辉三角2_组合数规律

---
+++ LeetCode/119_杨辉三角2.cpp
@@ -0,0 +1,44 @@
+/* 
+2021年02月12日13:56:42 大年初一来思考一下杨辉三角
+2021年02月12日14:02:23 有点没想出来，看答案
+
+2021年02月12日14:12:09 妙啊
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/pascals-triangle-ii/solution/yang-hui-san-jiao-ii-by-leetcode-solutio-shuk/
+
+
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, a, b) for(int i = int(a); i <= int(b); ++i)
+#define per(i, b, a) for(int i = int(b); i >= int(a); --i)
+#define mem(x, y) memset(x, y, sizeof(x))
+#define SZ(x) x.size()
+#define mk make_pair
+#define pb push_back
+#define fi first
+#define se second
+const ll mod=1000000007;
+const int inf = 0x3f3f3f3f;
+inline int rd(){char c=getchar();int x=0,f=1;while(c<'0'||c>'9'){if(c=='-')f=-1;c=getchar();}while(c>='0'&&c<='9'){x=x*10+c-'0';c=getchar();}return x*f;}
+inline ll qpow(ll a,ll b){ll ans=1%mod;for(;b;b>>=1){if(b&1)ans=ans*a%mod;a=a*a%mod;}return ans;}
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> row(rowIndex + 1);
+        row[0] = 1;
+        for (int i = 1; i <= rowIndex; ++i) {
+            row[i] = 1LL * row[i - 1] * (rowIndex - i + 1) / i;
+        }
+        return row;
+    }
+};
+
+
+int main(){
+    
+    
+    return 0;
+}
