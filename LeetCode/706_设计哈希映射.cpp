1dc3dc53a00a9f070f10bf9ef71d8f05608fe76a
shanlinmin <shanlinmin@qq.com>
2021-03-14 11:24:37 +0800

    [ADD] 简单实现hashtable

---
+++ LeetCode/706_设计哈希映射.cpp
@@ -0,0 +1,85 @@
+/* 
+2021年03月14日11:11:44 想了一会,难道要自己实现一个树？
+或者是两个链表？插入删除获取都要O(n)
+2021年03月14日11:15:10 看答案吧
+
+链接：https://leetcode-cn.com/problems/design-hashmap/solution/she-ji-ha-xi-ying-she-by-leetcode-soluti-klu9/
+发现答案就是模拟实现hash表，链表上面挂链表. 尴尬，我没想到
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, a, b) for(int i = int(a); i <= int(b); ++i)
+#define per(i, b, a) for(int i = int(b); i >= int(a); --i)
+#define mem(x, y) memset(x, y, sizeof(x))
+#define SZ(x) x.size()
+#define mk make_pair
+#define pb push_back
+#define fi first
+#define se second
+const ll mod=1000000007;
+const int inf = 0x3f3f3f3f;
+inline int rd(){char c=getchar();int x=0,f=1;while(c<'0'||c>'9'){if(c=='-')f=-1;c=getchar();}while(c>='0'&&c<='9'){x=x*10+c-'0';c=getchar();}return x*f;}
+inline ll qpow(ll a,ll b){ll ans=1%mod;for(;b;b>>=1){if(b&1)ans=ans*a%mod;a=a*a%mod;}return ans;}
+
+class MyHashMap {
+private:
+    vector<list<pair<int, int>>> data;
+    static const int base = 769;
+    static int hash(int key) {
+        return key % base;
+    }
+public:
+    /** Initialize your data structure here. */
+    MyHashMap(): data(base) {}
+    
+    /** value will always be non-negative. */
+    void put(int key, int value) {
+        int h = hash(key);
+        for (auto it = data[h].begin(); it != data[h].end(); it++) {
+            if ((*it).first == key) {
+                (*it).second = value;
+                return;
+            }
+        }
+        data[h].push_back(make_pair(key, value));
+    }
+    
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    int get(int key) {
+        int h = hash(key);
+        for (auto it = data[h].begin(); it != data[h].end(); it++) {
+            if ((*it).first == key) {
+                return (*it).second;
+            }
+        }
+        return -1;
+    }
+    
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    void remove(int key) {
+        int h = hash(key);
+        for (auto it = data[h].begin(); it != data[h].end(); it++) {
+            if ((*it).first == key) {
+                data[h].erase(it);
+                return;
+            }
+        }
+    }
+};
+
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
+
+int main(){
+    
+    
+    return 0;
+}
