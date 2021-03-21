5261286e68cc028885149ed0d69c7708ea5fe016
shanlinmin <shanlinmin@qq.com>
2021-03-21 13:31:20 +0800

    [ADD] lc74-原地矩阵置0

---
+++ LeetCode/73_矩阵置零.cpp
@@ -0,0 +1,107 @@
+/* 
+2021年03月21日13:12:56 
+感觉就是遍历一下，存0的位置，然后dfs
+但是这个可能导致O(n*m/2)的空间复杂度...
+
+看了答案，恍然
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/set-matrix-zeroes/solution/ju-zhen-zhi-ling-by-leetcode-solution-9ll7/
+
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, a, b) for(int i = int(a); i <= int(b); ++i)
+#define per(i, b, a) for(int i = int(b); i >= int(a); --i)
+#define mem(x, y) memset(x, y, sizeof(x))
+#define SZ(x) x.size()
+#define mk make_pair
+#define pb push_back
+#define fi first
+#define se second
+const ll mod=1000000007;
+const int inf = 0x3f3f3f3f;
+inline int rd(){char c=getchar();int x=0,f=1;while(c<'0'||c>'9'){if(c=='-')f=-1;c=getchar();}while(c>='0'&&c<='9'){x=x*10+c-'0';c=getchar();}return x*f;}
+inline ll qpow(ll a,ll b){ll ans=1%mod;for(;b;b>>=1){if(b&1)ans=ans*a%mod;a=a*a%mod;}return ans;}
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int flag_col0 = false, flag_row0 = false;
+        for (int i = 0; i < m; i++) {
+            if (!matrix[i][0]) {
+                flag_col0 = true;
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            if (!matrix[0][j]) {
+                flag_row0 = true;
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (!matrix[i][j]) {
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (!matrix[i][0] || !matrix[0][j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (flag_col0) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        if (flag_row0) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+    }
+};
+
+
+class Solution_hard_read {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int flag_col0 = false;
+        for (int i = 0; i < m; i++) {
+            if (!matrix[i][0]) {
+                flag_col0 = true;
+            }
+            for (int j = 1; j < n; j++) {
+                if (!matrix[i][j]) {
+                    // 如果i = 0时有，这matrix[0][0]为0，就用第一个数记录了第一行
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+        // 倒序防止了matrix[0][0]被提前更新
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = 1; j < n; j++) {
+                if (!matrix[i][0] || !matrix[0][j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+            if (flag_col0) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
+
+int main(){
+    
+    
+    return 0;
+}
