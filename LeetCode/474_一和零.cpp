1b30953f7146f523b6c1b4b5c6b8b59bf21574cd
shanlinmin <shanlinmin@qq.com>
2021-06-06 16:38:06 +0800

    [ADD] lc-474-01背包简化

---
+++ LeetCode/474_一和零.cpp
@@ -0,0 +1,64 @@
+/* 
+2021年06月06日15:30:59 感觉直接遍历计数，好像也没有问题
+可能需要去重
+
+2021年06月06日15:55:53 发现自己看错了题目，是整个子集的0和1的数量
+而不是子集中的每个元素...
+
+那就是01背包的变种
+
+2021年06月06日16:35:57 学了这么久算法，对于01背包的dp还是不够熟练...惭愧
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/ones-and-zeroes/solution/yi-he-ling-by-leetcode-solution-u2z2/
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, a, b) for(int i = int(a); i <= int(b); ++i)
+#define per(i, b, a) for(int i = int(b); i >= int(a); --i)
+#define mem(x, y) memset(x, y, sizeof(x))
+#define SZ(x) x.size()
+#define mk make_pair
+#define pb push_back
+#define fi first
+#define se second
+const ll mod=1000000007;
+const int inf = 0x3f3f3f3f;
+inline int rd(){char c=getchar();int x=0,f=1;while(c<'0'||c>'9'){if(c=='-')f=-1;c=getchar();}while(c>='0'&&c<='9'){x=x*10+c-'0';c=getchar();}return x*f;}
+inline ll qpow(ll a,ll b){ll ans=1%mod;for(;b;b>>=1){if(b&1)ans=ans*a%mod;a=a*a%mod;}return ans;}
+
+class Solution {
+public:
+    vector<int> getZerosOnes(string& str) {
+        vector<int> zerosOnes(2);
+        int length = str.length();
+        for (int i = 0; i < length; i++) {
+            zerosOnes[str[i] - '0']++;
+        }
+        return zerosOnes;
+    }
+
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        int length = strs.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+        for (int i = 0; i < length; i++) {
+            vector<int>&& zerosOnes = getZerosOnes(strs[i]);
+            int zeros = zerosOnes[0], ones = zerosOnes[1];
+            for (int j = m; j >= zeros; j--) {
+                for (int k = n; k >= ones; k--) {
+                    dp[j][k] = max(dp[j][k], dp[j - zeros][k - ones] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
+
+
+int main(){
+    
+    
+    return 0;
+}
